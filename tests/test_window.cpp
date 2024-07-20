6f260e7d22020842ddb08f9f34482961767f9b3c
bv7dev <bv7web@gmail.com>
2024-07-21 02:26:25 +0700

    move tests dir to project root

--- tests/test_window.cpp
+++ tests/test_window.cpp
@@ -1,12 +1,12 @@
-#include "lab_window.h"
-
 #include <iostream>
+
+#include "lab_window.h"
 
 int main() {
   lab::Window first_window{640, 400, "My window"};
   lab::Window second_window{300, 200, "Test"};
 
-  second_window.set_callback([&](int key, int scancode, int action, int mod) {
+  second_window.set_key_callback([&](int key, int scancode, int action, int mod) {
     if (key == 32 && action == 0 && mod == 0x0001) {
       first_window.~Window();
       std::cout << "test " << scancode << std::endl;
