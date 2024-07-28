4cc81be3430c40a1cc98cb18444fa2fc86593f26
bv7dev <bv7web@gmail.com>
2024-07-28 20:27:15 +0700

    experimenting with somewhat user-friendly buffer interface

--- src/headers/lab_buffer.h
+++ src/headers/lab_buffer.h
@@ -3,78 +3,127 @@
 
 #include "lab_webgpu.h"
 
-#include <webgpu/webgpu.hpp>
-
+#include <functional>
 #include <vector>
 
 namespace lab {
 
-struct ReadMap {
-  const void* data = nullptr;
-};
+template<class T> //<class BufType, size_t NumElems>
+struct ReadableBuffer {
+  using WriteCallback = std::function<void(std::vector<T>&)>;
+  using ReadCallback = std::function<void(const std::vector<T>&)>;
 
-struct WriteMap {
-  void* data = nullptr;
-};
+  // ReadableBuffer(Webgpu& inst, WriteCallback write_callback) : webgpu{inst} {
+  //   wgpu::BufferDescriptor bufferDesc;
+  //   bufferDesc.label = "My Buffer";
+  //   bufferDesc.usage = wgpu::BufferUsage::MapRead |
+  //   wgpu::BufferUsage::CopyDst; bufferDesc.size = sizeof(T) * NumElems;
+  //   bufferDesc.mappedAtCreation = true;
 
-struct Buffer {
-  Buffer(size_t bytes, Webgpu& wg) : webgpu{wg}, size{bytes} {
+  //   wgpu_buffer = webgpu.device.createBuffer(bufferDesc);
+
+  //   void* map = wgpu_buffer.getMappedRange(0, sizeof(T) * data.size());
+
+  //   std::vector<BufType> vmap{map, map + sizeof(T) * NumElems};
+
+  //   write_callback(vmap);
+
+  //   wgpu_buffer.unmap();
+  // }
+
+  ReadableBuffer(Webgpu& wg, const std::vector<T>& data) : webgpu{wg} {
     wgpu::BufferDescriptor bufferDesc;
     bufferDesc.label = "My Buffer";
     bufferDesc.usage = wgpu::BufferUsage::MapRead | wgpu::BufferUsage::CopyDst;
-    bufferDesc.size = bytes;
-    bufferDesc.mappedAtCreation = false;
+    bufferDesc.size = sizeof(T) * data.size();
+    bufferDesc.mappedAtCreation = true;
+
     wgpu_buffer = webgpu.device.createBuffer(bufferDesc);
-    // write_map.data = wgpu_buffer.getMappedRange(0, size);
-  }
 
-  void write(uint64_t offset, size_t length, const void* data) {
-    assert(length <= size);
-    webgpu.queue.writeBuffer(wgpu_buffer, offset, data, length);
-  }
-
-  WriteMap& get_write_map() {
-    if (!write_map.data) {
-      cbfunc = wgpu_buffer.mapAsync(
-          wgpu::MapMode::Write, 0, size,
-          [&](wgpu::BufferMapAsyncStatus status) {
-            std::cout << "buffer mapped with status: " << status << std::endl;
-            write_map.data = wgpu_buffer.getMappedRange(0, size);
-          });
-    }
-    return write_map;
-  }
-
-  const ReadMap& get_read_map() {
-    if (!read_map.data) {
-      cbfunc = wgpu_buffer.mapAsync(
-          wgpu::MapMode::Read, 0, size, [&](wgpu::BufferMapAsyncStatus status) {
-            std::cout << "buffer mapped with status: " << status << std::endl;
-            read_map.data = wgpu_buffer.getConstMappedRange(0, size);
-          });
-    }
-    return read_map;
-  }
-
-  void unmap() {
-    write_map.data = nullptr;
-    read_map.data = nullptr;
+    void* map = wgpu_buffer.getMappedRange(0, sizeof(T) * data.size());
+    memcpy(map, data.data(), sizeof(T) * data.size());
     wgpu_buffer.unmap();
   }
 
-  ~Buffer() {
+  struct MapBufferFuture {
+    std::vector<T> data{};
+    bool is_ready() { return size != 0 && data.size() != 0; }
+    size_t offset = 0, size = 0;
+  } _internal_map;
+
+  const MapBufferFuture& read_async(size_t offset, size_t size,
+                                    ReadCallback user_callback) {
+    std::cout << "read_async call..." << std::endl;
+
+    _internal_map.offset = offset;
+    _internal_map.size = size;
+
+    WGPUBufferMapCallbackInfo2 cbinfo{};
+    cbinfo.mode = wgpu::CallbackMode::AllowSpontaneous;
+    cbinfo.userdata1 = this;
+    cbinfo.userdata2 = &user_callback;
+    cbinfo.callback = [](WGPUMapAsyncStatus status, char const* msg,
+                         void* userdata1, void* userdata2) {
+      std::cout << "Buffer mapped with status: " << status << std::endl;
+      if (status != WGPUMapAsyncStatus_Success) {
+        std::cout << "Error: read_async: mapping failed!" << std::endl;
+        return;
+      }
+
+      ReadableBuffer* self = reinterpret_cast<ReadableBuffer*>(userdata1);
+
+      const void* buf = self->wgpu_buffer.getConstMappedRange(
+          sizeof(T) * self->_internal_map.offset,
+          sizeof(T) * self->_internal_map.size);
+
+      self->_internal_map.data.resize(self->_internal_map.size);
+      memcpy(self->_internal_map.data.data(), buf,
+             sizeof(T) * self->_internal_map.size);
+
+      (*reinterpret_cast<ReadCallback*>(userdata2))(self->_internal_map.data);
+
+      self->wgpu_buffer.unmap();
+
+      // if (*userdata2) {
+      //   std::vector<T> test{1, 2, 3, 4};
+      //   (*reinterpret_cast<ReadCallback*>(userdata2))(test);
+      // }
+
+      // MapBufferFuture* imap =
+      // reinterpret_cast<MapBufferFuture*>(userdata1);
+
+      // memcpy(imap->data, vmap, sizeof(T) * 3);
+      // imap->is_ready = true;
+
+      // Todo: I don't know yet what this msg parameter is
+      if (msg) std::cout << "read_async: message: " << msg << std::endl;
+    };
+
+    wgpu_buffer.mapAsync2(wgpu::MapMode::Read, sizeof(T) * offset,
+                          sizeof(T) * size, cbinfo);
+
+    // wgpuBufferMapAsync2(wgpu_buffer, WGPUMapMode_Read, sizeof(T) * offset,
+    //                     sizeof(T) * size, cbinfo);
+
+    return _internal_map;
+  }
+
+  void unmap() {
+    read_map = nullptr;
+    wgpu_buffer.unmap();
+  }
+
+  ~ReadableBuffer() {
     if (wgpu_buffer) {
       wgpu_buffer.release();
       wgpu_buffer = nullptr;
     }
   }
-  std::unique_ptr<wgpu::BufferMapCallback> cbfunc;
+
   wgpu::Buffer wgpu_buffer;
-  const size_t size;
   Webgpu& webgpu;
 
-  WriteMap write_map;
-  ReadMap read_map;
+  const void* read_map = nullptr;
 };
 
 } // namespace lab
--- tests/test_buffer.cpp
+++ tests/test_buffer.cpp
@@ -10,28 +10,36 @@ int main() {
   lab::Surface surface{window, webgpu};
   lab::Pipeline pipeline{shader, webgpu};
 
-  lab::Buffer testbuf{1024, webgpu};
+  std::vector<int> message{1, 2, 3, 4, 5, 6, 7, 8};
+  lab::ReadableBuffer buffer{webgpu, message};
 
-  const size_t len = 20;
-  const uint8_t msg[len] = "This is a Warning!\n";
+  auto test = buffer.read_async(2, 3, [](const std::vector<int>& data) {
+    std::cout << "callback: ";
+    for (auto d : data) {
+      std::cout << d << ", ";
+    }
+    std::cout << std::endl;
+  });
 
-  testbuf.write(0, len, msg);
-
-  // lab::WriteMap& wmap = testbuf.get_write_map();
-  // for (int i = 0; i < len; ++i) {
-  //   reinterpret_cast<uint8_t*>(wmap.data)[i] = msg[i];
-  // }
-  // testbuf.unmap();
-
-  const lab::ReadMap& bufmap = testbuf.get_read_map();
+  // const void* const* bufmap = buffer.get_read_map(0, len);
 
   while (lab::tick()) {
-    if (bufmap.data) {
-      std::cout << (const char*)bufmap.data << std::endl;
-      glfwSetWindowTitle((GLFWwindow*)window.get_handle(),
-                         (const char*)bufmap.data);
-      testbuf.unmap();
+    if (test.is_ready()) {
+      std::cout << "ready" << std::endl;
+
+      for (auto d : test.data) {
+        std::cout << d << ", ";
+      }
+      std::cout << std::endl;
+
+      test.data.clear();
     }
+    // if (*bufmap) {
+    //   std::cout << (const char*)(*bufmap) << std::endl;
+    //   glfwSetWindowTitle((GLFWwindow*)window.get_handle(),
+    //                      (const char*)(*bufmap));
+    //   buffer.unmap();
+    // }
     pipeline.render_frame(surface);
     lab::sleep(16ms);
   }
