d9d3297d986a3767ce5db137c2140929e0f4ce1e
bv7dev <bv7web@gmail.com>
2024-07-31 02:54:20 +0700

    cleanup sample

--- executables/sample_buffer.cpp
+++ executables/sample_buffer.cpp
@@ -1,61 +1,46 @@
 #include <lab>
 
+using namespace std;
+using namespace lab;
+
 int main() {
-  lab::Webgpu webgpu("My Instance");
+  Webgpu webgpu("My Instance");
 
-  // Init Buffer ---------------------------------------------------------------
+  // Write Buffer --------------------------------------------------------------
   bool writing_done = false;
-  std::cout << "\n\nWriting Buffer...\n";
-  lab::ReadableBuffer<std::string> buffer("My Buffer", webgpu);
+  cout << "\n\nWriting Buffer...\n";
+  ReadableBuffer<int> buffer("My Buffer", webgpu);
 
-  auto init_buffer = [&writing_done](lab::MappedVRAM<std::string>&& vmap) {
+  auto init_buffer = [&writing_done](MappedVRAM<int>&& vmap) {
     int start_point = 6;
     vmap.resize(start_point);
     for (int i = 0; i < 100; ++i) {
-      lab::sleep(10ms); // simulate slow loading, converting, etc.
-      std::cout << vmap.push("bv7-" + (i + 5)) << " ";
+      cout << vmap.push(i + 5) << " "; // push some numbers
     }
-    // unmap CAN be called when done reading or writing, otherwise
-    vmap.unmap(); // it will be automatically unmapped on return
-
-    std::cout << "\nbuffer initialized\n";
+    cout << "\nbuffer initialized" << endl;
     writing_done = true;
   };
   buffer.to_device(256, init_buffer);
 
-  std::cout << "Main thread waiting...";
-  while (!writing_done) {
-    lab::sleep(20ms);
-    std::cout << ".";
-  }
-
-  // this buffer does nothing, it's just to see if something breaks
-  lab::ReadableBuffer<std::string> another("Stress Test",
-                                           std::vector<std::string>{"1", "2", "3", "4+"}, webgpu);
+  cout << "Main thread waiting...";
+  while (!writing_done)
+    cout << '.';
 
   // Read buffer ---------------------------------------------------------------
   bool reading_done = false;
-  std::cout << "\n\nReading Buffer...\n";
-  auto read_buffer = [&reading_done](lab::ConstMappedVRAM<std::string>&& vmap) {
+  cout << "\n\nReading Buffer...\n";
+  auto read_buffer = [&reading_done](ConstMappedVRAM<int>&& vmap) {
     for (auto e : vmap) {
-      lab::sleep(50ms); // simulate slow processing of received data
-      std::cout << e << " ";
+      cout << e << " "; // print some numbers received from gpu
     }
-    std::cout << std::endl;
+    cout << "\nbuffer read" << endl;
     reading_done = true;
   };
   buffer.from_device(2, 200, read_buffer);
 
-  std::cout << "Main thread waiting...";
-  while (!reading_done) {
-    lab::sleep(10ms);
-    std::cout << ".";
-  }
-  std::cout << "\nFIN !!!\n" << std::endl;
+  cout << "Main thread waiting...";
+  while (!reading_done)
+    cout << ".";
 
-  reading_done = false;
-  another.from_device(read_buffer);
-  while (!reading_done) {
-  }
-  // end stress test
+  cout << "\nFIN !!!\n" << endl;
 }
