827cb7f971d8554b610be77af0315cd8e6ed6cac
bv7dev <bv7web@gmail.com>
2024-07-31 20:21:28 +0700

    start learning about vertex buffers

---
+++ executables/learn_vertex_buffers.cpp
@@ -0,0 +1,19 @@
+#include <lab>
+using namespace lab;
+
+int main() {
+  Webgpu webgpu("My Instance");
+  Shader shader("My Shader", "shaders/test1.wgsl");
+  Window window("A Window and a Triangle", 640, 400);
+
+  Surface surface(window, webgpu);
+  Pipeline pipeline(shader, webgpu);
+
+  window.set_resize_callback(
+      [&surface](int width, int height) { surface.reconfigure(width, height); });
+
+  while (tick()) {
+    pipeline.render_frame(surface, {3, 1});
+    lab::sleep(50ms);
+  }
+}
