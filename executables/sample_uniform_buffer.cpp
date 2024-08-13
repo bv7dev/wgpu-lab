c33362543c6f4075aed16f0efe4a266422544eb6
bv7dev <bv7web@gmail.com>
2024-08-14 00:29:26 +0200

    small adjustments

--- executables/sample_uniform_buffer.cpp
+++ executables/sample_uniform_buffer.cpp
@@ -11,12 +11,12 @@ struct MyUniforms {
 };
 
 int main() {
-  lab::Webgpu webgpu("My Instance");
   lab::Window window("Vertex Buffer & Uniforms Demo", 640, 400);
 
   std::vector<MyVertex> vertex_data = {
       {0.f, 1.f}, {-sqrtf(3.f) / 2.f, -3.f / 6.f}, {sqrtf(3.f) / 2.f, -3.f / 6.f}};
 
+  lab::Webgpu webgpu("My Instance");
   lab::Buffer<MyVertex> vertex_buffer("My vertex buffer", vertex_data, webgpu);
 
   MyUniforms uniforms{.ratio = {window.ratio(), 1.0f}, .time = 0.0f, .scale = 0.4f};
@@ -37,28 +37,31 @@ int main() {
   lab::Surface surface(window, webgpu);
 
   std::thread time_resetter{[]() {
-    std::cout << "Info: spawned a thread which resets time every 7 seconds" << std::endl;
+    std::cout << "Info: time_resetter: spawned a thread which resets time roughly every 7 seconds"
+              << std::endl;
     while (true) {
-      lab::reset_time();
-      while (lab::elapsed_seconds() < 7.0f) {
-        lab::sleep(20ms);
-      }
       std::cout << "time reset at: " << lab::elapsed_seconds() << std::endl;
+      lab::reset_time();
+      while (lab::elapsed_seconds() < 6.997f) {
+        // when accuracy doesn't matter, save system resources
+        lab::sleep(10ms);
+      }
     }
   }};
 
   while (lab::tick()) {
-    // send uniforms to gpu
+    // send uniform data to gpu
     uniform_buffer.write(uniforms);
 
-    // render the scene
+    // render the scene to surface
     pipeline.render_frame(surface, {3, 1});
 
-    // update uniform values
+    // update uniform values for next draw call
     uniforms.ratio[0] = window.ratio();
     uniforms.time = lab::elapsed_seconds();
 
-    lab::sleep(20ms);
+    // sacrifice fps to save battery life
+    lab::sleep(10ms);
   }
 
   time_resetter.join();
