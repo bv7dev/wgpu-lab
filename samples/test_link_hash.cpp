ab0f60e0c315d826e5c3b9a42006000bf301086a
bv7dev <bv7web@gmail.com>
2024-09-09 12:31:55 +0200

    .

--- samples/test_link_hash.cpp
+++ samples/test_link_hash.cpp
@@ -1,9 +1,14 @@
 #include <format>
 #include <iostream>
+#include <string>
 
+#pragma region // 4 and 8 bit unsigned types
 struct ui4 {
+  ui4() = default;
   ui4(std::integral auto v) : value{uint8_t(v & 0xfu)} {}
   operator uint8_t() const { return value; }
+  uint8_t operator++() { return ++value; }
+  uint8_t operator++(int) { return value++; }
   uint8_t value;
 };
 template<>
@@ -13,9 +18,14 @@ struct std::formatter<ui4> : std::formatter<std::string> {
 std::ostream& operator<<(std::ostream& o, ui4 i) { return o << unsigned(i.value); }
 
 struct ui8 {
+  ui8() = default;
   ui8(const ui4& high, const ui4& low) : value{uint8_t(high.value << 4 | low.value)} {}
+  ui8(const ui4& low) : value{low.value} {}
   ui8(std::integral auto v) : value{uint8_t(v)} {}
   operator uint8_t() const { return value; }
+  operator ui4() const { return ui4(value); }
+  uint8_t operator++() { return ++value; }
+  uint8_t operator++(int) { return value++; }
   ui4 high() { return ui4(value >> 4); }
   ui4 low() { return ui4(value); }
   uint8_t value;
@@ -25,25 +35,17 @@ struct std::formatter<ui8> : std::formatter<std::string> {
   auto format(ui8 i, format_context& ctx) const { return formatter<string>::format(std::format("{}", i.value), ctx); }
 };
 std::ostream& operator<<(std::ostream& o, ui8 i) { return o << unsigned(i.value); }
+#pragma endregion
 
 struct Link {
   ui8 key;
 };
 
 int main() {
-  using namespace std;
-
-  ui4 a = 5;
-  ui4 b = 35;
-  ui8 c{b, a};
-  ui8 d{a, b};
-
-  uint8_t x = c + 3;
-  uint8_t y = ui8(a, b);
-  uint8_t z = a + b;
-  ui4 w = a + d;
-
-  std::cout << w << c << a << std::endl;
-  cout << format("{}, {}, {}, {}\n", a, b, c, d);
-  cout << format("{}, {}, {}, {}\n", x, y, z, w);
+  for (ui8 a = 0; a < 64; ++a) {
+    if (a.high() > 0) {
+      std::cout << a.high() << '.';
+    }
+    std::cout << ui4(a) << "   ";
+  }
 }
