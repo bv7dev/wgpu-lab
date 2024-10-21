4448382ca4286ed209eef2162994c345741bac39
bv7dev <bv7web@gmail.com>
2024-10-21 20:05:11 +0200

    py script to clone submodules attempt static link to dawn

---
+++ scripts/fetch_lab_dependencies.py
@@ -0,0 +1,15 @@
+import os
+import subprocess
+
+def git_clone(repo_url, branch_or_tag_name=None):
+  args = ['git', 'clone', repo_url]
+  if branch_or_tag_name is not None:
+    args.extend(['-b', branch_or_tag_name])
+  subprocess.run(args)
+
+if __name__ == '__main__':
+  os.mkdir('libs/')
+  os.chdir('libs/')
+  git_clone('https://dawn.googlesource.com/dawn', 'chromium/6670')
+  git_clone('https://github.com/g-truc/glm', '1.0.1')
+  git_clone('https://github.com/syoyo/tinygltf', 'v2.9.3')
