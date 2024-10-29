00b998495f316849e11870303a76c222bb4544cd
bv7dev <bv7web@gmail.com>
2024-10-29 20:18:48 +0100

    automatically fetch dependencies on cmake configure

--- scripts/lab_fetch_dependencies.py
+++ scripts/lab_fetch_dependencies.py
@@ -1,12 +1,25 @@
-from lab_helpers import *
+import os
+import sys
 
-if dir_create_then_enter('libs'):
-  print("fetching wgpu-labs's dependencies... \n")
+from lab_helpers import dir_create_then_enter, git_clone
 
-  git_clone('https://dawn.googlesource.com/dawn', 'chromium/6670')
-  git_clone('https://github.com/g-truc/glm', '1.0.1')
-  git_clone('https://github.com/syoyo/tinygltf', 'v2.9.3')
+try:
+  assert len(sys.argv) == 3, 'Not enough arguments'
+  assert sys.argv[1] == '--root', 'Missing "--root" argument'
 
-else:
-  print("wgpu-lab's dependencies already fetched")
-  print("delete libs directory to reset")
+  os.chdir(sys.argv[2]) # change into wgpu-lab's root directory
+
+  if dir_create_then_enter('libs'):
+    print("fetching wgpu-labs's dependencies... \n")
+
+    git_clone('https://dawn.googlesource.com/dawn', 'chromium/6670')
+    git_clone('https://github.com/g-truc/glm', '1.0.1')
+    git_clone('https://github.com/syoyo/tinygltf', 'v2.9.3')
+
+  else:
+    print("already got wgpu-lab's dependencies")
+    print("if broken, delete libs directory to reset")
+
+except AssertionError as err:
+  print('Error:', err)
+  print('Usage: "python scripts/lab_fetch_dependencies.py --root [LAB_ROOT_DIR]"')
